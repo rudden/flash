e71173660f7d363003a34e1c01f5a5874a7eb92c
SIMON RUDÉN <siru14@sweet.student.bth.se>
2015-04-23 19:52:58 +0200

    remove view

--- view/flash/index.tpl.php
+++
@@ -1,8 +0,0 @@
-<form method="post">
-	<input type="submit" name="info" value="Info Message" onClick="this.form.action = '<?= $this->url->create('flash/choice') ?>'" />
-	<input type="submit" name="error" value="Error Message" onClick="this.form.action = '<?= $this->url->create('flash/choice') ?>'" />
-	<input type="submit" name="success" value="Success Message" onClick="this.form.action = '<?= $this->url->create('flash/choice') ?>'">
-	<input type="submit" name="warning" value="Warning Message" onClick="this.form.action = '<?= $this->url->create('flash/choice') ?>'">
-
-	<output><?= $output ?></output>
-</form>
